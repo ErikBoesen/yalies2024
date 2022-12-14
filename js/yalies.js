a4c2da74f9a792c6c2d4815148fde1b8d1d16b38
ErikBoesen <me@erikboesen.com>
2022-12-14 15:26:18 -0500

    Complete Yalies page

--- js/yalies.js
+++ js/yalies.js
@@ -1,6 +1,10 @@
 const API_ENDPOINT = 'https://yalies.io/api/people',
       API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzEwNDg0NTUsInN1YiI6ImVrYjMzIn0.0xYhLZiBSSr0IrRXfOISWRLKqpru8jTcSws5jTiU91M';
 
+const elem = {
+    people: document.getElementById('people'),
+};
+
 fetch(API_ENDPOINT, {
     method: 'POST',
     headers: {
@@ -15,6 +19,17 @@ fetch(API_ENDPOINT, {
     }),
 })
 .then(response => response.json())
-.then(json => {
-    console.log(json);
+.then(people => {
+    for (let person of people) {
+        console.log(person);
+        let container = document.createElement('div');
+        container.className = 'person';
+        container.innerHTML = ('<img src="' + (person.image || 'https://yalies.io/static/images/user.png') + '">' +
+            '<h4>' + person.first_name + ' ' + person.last_name + '</h4>' +
+            '<p>College: ' + person.college + '</p>' +
+            '<p>Major: ' + person.major + '</p>' +
+            '<p>Birthday: ' + person.birthday + '</p>' +
+            '<p>NetID: ' + person.netid + '</p>');
+        elem.people.appendChild(container);
+    }
 });
