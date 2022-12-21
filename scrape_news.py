96c2b8d51939c0bb6deba200f5f2f765d54e6c81
ErikBoesen <me@erikboesen.com>
2022-12-21 14:33:30 -0500

    Write articles as YAML

--- scrape_news.py
+++ scrape_news.py
@@ -1,19 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import yaml
 
 HOST = 'https://news.yale.edu'
 url = HOST + '/search?sort=created&order=desc&search_api_views_fulltext='
 soup = BeautifulSoup(requests.get(url).text, "html.parser").find('div', {'id': 'block-system-main'})
 # TODO: protect against exact link index being changed
 rows = soup.find_all('div', {'class': 'views-row'})
+articles = []
 for row in rows:
     link = row.find('a')
     url = HOST + link['href']
     title = link.text
     date = row.find('div', {'class': 'date'}).text
     teaser = row.find('div', {'class': 'teaser'}).text
-    print(url)
-    print(title)
-    print(date)
-    print(teaser)
-    print()
+    articles.append({
+        'url': url,
+        'title': title.strip(),
+        'date': date.strip(),
+        'teaser': teaser.strip(),
+    })
+
+with open('_data/articles.yml', 'w') as f:
+    yaml.dump(articles, f)
